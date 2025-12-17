f6beb4a75a6a3f7ccf8dcde00c93738216745728
dimplekalra <dimplekalra2711@gmail.com>
2025-12-18 04:13:33 +0530

    Added frontend url string for deployment

--- task-tracker-backend/src/app.ts
+++ task-tracker-backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-domain.vercel.app",
+      "https://task-tracker-mu-gules.vercel.app",
     ],
     credentials: true,
   })
